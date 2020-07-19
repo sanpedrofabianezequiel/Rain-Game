195b8834c2888a9832eafd1abb22165276320c2c
ezequiel <ezequiel@ezequiel.com>
2020-07-18 22:50:17 -0300

    First Commit

---
+++ RainGame/UnLuncher/Rain Cube Game/Assets/Algoritmos/Movimiento.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Movimiento : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(Input.GetAxis("Horizontal"),0,0); //Set movimiento transverzal
+                                                                        //Los demas ejes se quedan inmoviles
+        
+         if(transform.position.x < -10)//Limite Izquierdo
+        {                               
+            transform.position = new Vector3(-10, -3);
+        }else if(transform.position.x > 10)//Limite Derecho
+        {
+            transform.position = new Vector3(10, -3);
+        }
+
+    }
+
+    //////////////////////////////////
+    ///Metodo colicionador
+    public void OnCollisionEnter()//Metodo por defecto
+    {
+        SceneManager.LoadScene("SampleScene");//vuelve a la escena
+    }
+
+
+
+}
---
+++ RainGame/UnLuncher/Rain Cube Game/Assets/Algoritmos/lluviaCubos.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lluviaCubos : MonoBehaviour
+{
+    public float Delay = 0.1f; //Es el retraso en que apresca un nuevo cubo
+    public GameObject cubox;    //Lo qye va a aprecer es un CUBO
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Inicio", Delay, Delay);//(metodo,tiempo demora,repeticion)
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Inicio()    //Metodo  Va crear un cubo nuevo en un lugar ALAZAR
+    {
+        Instantiate(cubox,  //Que objeto vamos a crear
+                        new Vector3 (Random.Range(-10,10),//En que posicion del eje X
+                            10,                             //En que posicion del eje Y
+                                0),                            //En el EJE Z
+                                    Quaternion.identity );       //  
+      
+
+            
+    }
+
+}
